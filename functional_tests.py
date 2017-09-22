e3ccb3fcf52a0929a9e7b060dacc4813cd00c309
Evan Sanford <evwhiz@gmail.com>
2017-09-22 15:30:13 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,8 @@ Functional tests
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -21,27 +23,43 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # He is invited to enter a to-do item staight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
 
-# He types "Buy peacock feathers" into a text box (Evan's hobby
-# is tying fly-fishing lures)
+        # He types "Buy peacock feathers" into a text box (Evan's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-# When he hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # When he hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# There is still a text box inviting him to add another item.  He
-# enters "use peacock feathers to make a fly" (Evan is very methodical)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                        any(row.text ==
+                            '1: Buy peacock feathers' for row in rows)
+                        )
 
-# The page updates again, and now shows both items on his list
+        # There is still a text box inviting him to add another item.  He
+        # enters "use peacock feathers to make a fly" (Evan is very methodical)
+        self.fail('Finish the test!')
 
-# Evan wonders whether the site will remember his list.  Then he sees
-# that the site has generated a unique URL for him -- there is some
-# explanatory text to that effect.
+        # The page updates again, and now shows both items on his list
 
-# He visits that URL - his to-do list is still there.
+        # Evan wonders whether the site will remember his list.  Then he sees
+        # that the site has generated a unique URL for him -- there is some
+        # explanatory text to that effect.
 
-# Satisfied, he goes back to sleep
+        # He visits that URL - his to-do list is still there.
+
+        # Satisfied, he goes back to sleep
 
 
 if __name__ == '__main__':
